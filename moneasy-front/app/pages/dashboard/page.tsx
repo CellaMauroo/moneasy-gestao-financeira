5ec3847337ead9085ff05696960c55f354f59e7f
Mauro Cella <maurocellaa3301@gmail.com>
2025-06-03 22:43:55 -0300

    Navbar and header components

--- moneasy-front/app/pages/dashboard/page.tsx
+++ moneasy-front/app/pages/dashboard/page.tsx
@@ -1,21 +1,37 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
+import Header from "../../components/header"
+import Navbar from "../../components/navbar"
+
 
 export default function DashboardPage() {
     const router = useRouter()
+    const [isChecking, setIsChecking] = useState(true)
 
     useEffect(() => {
         const isLogged = localStorage.getItem("logged") === "true"
         if (!isLogged) {
             router.push("/login")
+        } else {
+            setIsChecking(false)
         }
     }, [])
 
+    if (isChecking) return null
+
     return (
         <div>
-            <h1 className="text-7xl font-bold">Dashboard</h1>
+            <Header/>
+            <div className="flex">
+
+            <Navbar/>
+            <main className="w-6/7 p-6 bg-gray-300">
+                <h1 className="text-4xl font-bold">Dashboard</h1>
+            </main>
+            </div>
+
         </div>
     )
 }
