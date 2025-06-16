6e1ac3fb84b0e24b5014c5c43c856ab3f1983dd5
Mauro Cella <maurocellaa3301@gmail.com>
2025-06-16 18:33:51 -0300

    Forum page

---
+++ moneasy-front/app/components/forumCard.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react"
+
+interface ForumCardProps {
+  title: string
+  date: string
+  onClick?: () => void
+}
+
+const ForumCard: FC<ForumCardProps> = ({ title, date, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="bg-white flex justify-between items-start mb-4 rounded shadow-sm cursor-pointer hover:bg-gray-100 transition h-[100px]"
+    >
+      <div className="border-l-8 border-green-600 pl-4 h-full">
+        <h2 className="font-bold text-black mt-5">{title}</h2>
+        <p className="text-sm text-gray-500">Acessar fórum...</p>
+      </div>
+      <span className="text-sm text-gray-500 p-10">{date}</span>
+    </div>
+  )
+}
+
+export default ForumCard
